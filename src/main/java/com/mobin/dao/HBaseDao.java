418e54c512a311583b3b61b85dbce91e79dd66da
Mobin <996833461@qq.com>
2016-03-16 08:09:59 -0700

    HBaseDao

---
+++ src/main/java/com/mobin/dao/HBaseDao.java
@@ -0,0 +1,9 @@
+package com.mobin.dao;
+
+/**
+ * Created by hadoop on 3/15/16.
+ */
+public interface HBaseDao {
+
+    public void truncate();
+}
---
+++ src/main/java/com/mobin/dao/impl/HBaseDaoImpl.java
@@ -0,0 +1,57 @@
+package com.mobin.dao.impl;
+
+import com.mobin.dao.HBaseDao;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.springframework.data.hadoop.hbase.HbaseTemplate;
+import org.springframework.data.hadoop.hbase.TableCallback;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hadoop on 3/15/16.
+ */
+public class HBaseDaoImpl implements HBaseDao{
+
+    private HbaseTemplate htemplate;
+    private Admin admin;
+
+    public void truncate() {
+        Connection conn = null;
+        try {
+            conn = ConnectionFactory.createConnection(htemplate.getConfiguration());
+            List<TableName> tablenames = new ArrayList<TableName>();
+            tablenames.add(TableName.valueOf("TRAVEL"));
+            tablenames.add(TableName.valueOf("TRAVELSORTHOTEL"));
+            tablenames.add(TableName.valueOf("TRAVELSORTPRICE"));
+            for (TableName tn : tablenames) {
+                admin.disableTable(tn);
+                admin.truncateTable(tn,false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                admin.close();
+                conn.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+    public HbaseTemplate getHtemplate() {
+        return htemplate;
+    }
+
+    public void setHtemplate(HbaseTemplate htemplate) {
+        this.htemplate = htemplate;
+    }
+}
