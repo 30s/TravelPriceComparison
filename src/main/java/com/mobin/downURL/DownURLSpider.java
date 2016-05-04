54785f2daf7d2c05b586784ce842cc9fd5c5bf19
Mobin <996833461@qq.com>
2016-05-04 07:07:26 -0700

    DownURLSpider

--- src/main/java/com/mobin/downURL/DownURLSpider.java
+++ src/main/java/com/mobin/downURL/DownURLSpider.java
@@ -35,8 +35,7 @@ public class DownURLSpider implements PageProcessor{
 				 *2.出发点错误，目的地正确或错误----------title不匹配 
 				 * */	    	
 				String title = page.getHtml().xpath("//title").toString();
-				System.out.println(title);
-				
+
 				if(title != null && title.contains(start)){
 					end = getMatcher(".*kd/list_([\u4e00-\u9fa5]+)",page.getUrl().toString());
 					String url = "http://dujia.qunar.com/golfz/routeList/adaptors/pcTop?&isTouch=0&t=all&o=pop-desc&lm=0%2C20&fhLimit=0%2C20&q="+end+"&d="+start+"&s=all&qs_ts=1447834475346&ti=3&tm=l01_all_search_origin&sourcepage=list&qssrc=eyJ0cyI6IjE0NDc4MzQ0NzUzNDYiLCJzcmMiOiJhbGwuZW52YSIsImFjdCI6InNlYXJjaCJ9&m=l%2Clm&displayStatus=pc&lines6To10=0&_=1447834476708";
@@ -46,11 +45,9 @@ public class DownURLSpider implements PageProcessor{
 			
 			 String recordCount =  new JsonPathSelector("$.data.list.numFound")
 			 .select(page.getRawText());//总记录数
-			 System.out.println(recordCount+"aaaaaaaaaaaaaaaaaaaaaa");
 			 //如果总记录数小于20，就只有一页
 			 int pageCount = (((Integer.valueOf(recordCount))/20) < 0) ? 1 : (Integer.valueOf(recordCount))/20;
-			 int len = pageCount >= 3 ? 3 : pageCount;
-			 System.out.println(pageCount+"ppppppppppppppppppp");
+			 int len = pageCount >= 6 ? 6 : pageCount;
 			 int n = 0;
 			 for (int i = 0; i <len; i ++) {
 				 String currentURL = page.getUrl().toString();//当前的用于获取总页数URL，也是获取其他数据的URL
@@ -59,9 +56,7 @@ public class DownURLSpider implements PageProcessor{
 				 allPageURL.add(recordURL_);//一个出发点-目的地对应的所有页数的数据的URL
 				 n = n+20;
 			 }
-			 
-			 
-			 
+
 		 }
 		
 	}
