23454256aac288c11b4aec51efa52d4fcc164d59
Mobin <996833461@qq.com>
2016-05-14 04:47:16 -0700

    TravelDao

--- src/main/java/com/mobin/dao/TravelDao.java
+++ src/main/java/com/mobin/dao/TravelDao.java
@@ -6,17 +6,11 @@ import com.mobin.domain.Travel;
 
 public interface TravelDao {
 
-	public List findPageRecords(int currentPageNum, int pageSize,String ST,String SP,String EP);
-	public List findPageRecords(int startIndex, int pageSize,String ST);//管理员页面的数据分页
+	public List findPageRecords(int currentPageNum, int pageSize,String ST,String SP,String EP,String TDATA,String HGRADE,String sort);
+	//public List findPageRecords(int startIndex, int pageSize,String ST);//管理员页面的数据分页
 	public int getTotalRecords(String ST,String SP,String EP);
 	public int getTotalRecords(String SP);
 	public Boolean login(String user,String password);
-	public Boolean deleteRecordsByPlace(String place);
-
-	public List<Travel> sortByPrice(String flagHighORLow,int currentPageNum, int pageSize, String ST, String SP, String EP);
-	public List<Travel> sortByHotel(int hotellevel,int currentPageNum, int pageSize, String ST, String SP, String EP);
-
-	public Boolean deleteRecordsAll();
 
 	public void update();
 
