59ea97de712b0997746156c0b712e574a2d822ed
Mobin <996833461@qq.com>
2016-05-18 01:03:53 -0700

    DFSFile

---
+++ src/main/java/com/mobin/domain/DFSFile.java
@@ -0,0 +1,43 @@
+package com.mobin.domain;
+
+/**
+ * Created by hadoop on 5/18/16.
+ */
+public class DFSFile {
+    private String permission;
+    private String owner;
+    private Long size;
+    private String name;
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
