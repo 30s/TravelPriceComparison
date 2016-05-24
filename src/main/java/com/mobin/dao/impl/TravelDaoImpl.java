e9f00cd73da49b0cd9699f0b35ef0bbfc328cb53
Mobin <996833461@qq.com>
2016-05-24 07:59:50 -0700

    后台添加了交通的筛选的功能

--- src/main/java/com/mobin/dao/impl/TravelDaoImpl.java
+++ src/main/java/com/mobin/dao/impl/TravelDaoImpl.java
@@ -34,7 +34,7 @@ public class TravelDaoImpl implements TravelDao {
 
     //得到rowkey,startkey,endkey
     public void getXkey(int currentPageNum, int pageSize, String ST, String SP, String EP,String TDATA,String HGRADE,String sort
-            ,String firstPrice,String secondPrice) {
+            ,String firstPrice,String secondPrice,String TRAFFIC) {
         System.out.println(currentPageNum);
         System.out.println(pageSize);
         System.out.println(ST);
@@ -45,14 +45,30 @@ public class TravelDaoImpl implements TravelDao {
         System.out.println(sort);
 
         if (firstPrice != null && secondPrice != null) {
-             if (null == HGRADE && null == TDATA) {
+             if (null == HGRADE && null == TDATA && null == TRAFFIC) {
                 rk1 = SP + "PRICE" + EP + firstPrice + "%";
                 rk2 = SP + "PRICE" + EP + secondPrice + "%";
                 rowkey = SP + "PRICE" + EP + "%";
-             } else if (null == HGRADE) {
-                rk1 = SP + "EEE" + EP + TDATA + "天" + firstPrice + "%";
-                rk2 = SP + "EEE" + EP + TDATA + "天" + secondPrice + "%";
-                rowkey = SP + "EEE" + EP + TDATA + "天" + "%";
+             }
+             else if(null == HGRADE && null == TDATA){
+                 rk1 = SP + "HHH" + EP + TRAFFIC + firstPrice + "%";
+                 rk2 = SP + "HHH" + EP + TRAFFIC + secondPrice + "%";
+                 rowkey = SP + "HHH" + EP + TRAFFIC;
+             }
+             else if (null == HGRADE && null == TRAFFIC){
+                 rk1 = SP + "EEE" + EP + TDATA + "天" + firstPrice + "%";
+                 rk2 = SP + "EEE" + EP + TDATA + "天" + secondPrice + "%";
+                 rowkey = SP + "EEE" + EP + TDATA + "天" + "%";
+             }
+             else if (null == TDATA && null == TRAFFIC){
+                 rk1 = SP + "FFF" + EP + HGRADE + firstPrice + "%";
+                 rk2 = SP + "FFF" + EP + HGRADE + secondPrice + "%";
+                 rowkey = SP + "FFF" + EP + HGRADE + "%";
+             }
+             else if (null == HGRADE) {
+                rk1 = SP + "EEE" + EP + TRAFFIC + "%" +  TDATA + "天" + firstPrice + "%";
+                rk2 = SP + "EEE" + EP + TRAFFIC + "%" +  TDATA + "天" + secondPrice + "%";
+                rowkey = SP + "EEE" + EP + TRAFFIC + "%" +  TDATA + "天";
              } else if (null == TDATA) {
                 rk1 = SP + "FFF" + EP + HGRADE + firstPrice + "%";
                 rk2 = SP + "FFF" + EP + HGRADE + secondPrice + "%";
@@ -63,54 +79,70 @@ public class TravelDaoImpl implements TravelDao {
                 rowkey = SP + "AAA" + EP + TDATA + "天" + HGRADE + "%";
              }
              System.out.println(rk1);
-             startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVEL WHERE ROWKEY >= ?  LIMIT 1 ", Integer.class, rk1);
+             startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVELTESTS WHERE ROWKEY >= ?  LIMIT 1 ", Integer.class, rk1);
              startkey = startRecord + (currentPageNum - 1) * pageSize;
              endkey = startRecord + (currentPageNum) * pageSize;
-             findPageRecords_sql = "SELECT ROWKEY FROM TRAVEL WHERE ROWKEY >=  ? AND RECORDID >= ? AND RECORDID <= ? Limit 10";
+             findPageRecords_sql = "SELECT ROWKEY FROM TRAVELTESTS WHERE ROWKEY >= "+"'"+""+rk1+""+"'"+" AND ROWKEY <= "+"'"+""+rk2+""+"'"+" AND RECORDID >= ? AND RECORDID <= ?";
         }
 
         else {
             if (sort != null && !"".equals(sort)) { //按价格进行排序
-                if (null == HGRADE && null == TDATA)  //查询条件为出发点,目的地,时间，按价格排序
-                    rowkey = " ROWKEY LIKE  " + SP + "PRICE" + EP + "%";
-                else if (null == HGRADE)                 //查询条件为出发点,目的地,时间，出游天数,按价格排序
+                if (null == HGRADE && null == TDATA && TRAFFIC == null)  //查询条件为出发点,目的地,时间，按价格排序
+                    rowkey =  SP + "PRICE" + EP + "%";
+                else if(null == HGRADE && null == TDATA)
+                    rowkey = SP + "GGG" + EP + TRAFFIC + "%";
+                else if (null == HGRADE && null == TRAFFIC)                 //查询条件为出发点,目的地,时间，出游天数,按价格排序
                     rowkey = SP + "EEE" + EP + TDATA + "天%";
-                else if (null == TDATA)                   //查询条件为出发点,目的地,时间，酒店等级，按价格排序
+                else if (null == TDATA && null == TRAFFIC)                   //查询条件为出发点,目的地,时间，酒店等级，按价格排序
                     rowkey = SP + "FFF" + EP + HGRADE + "%";
-                else                                          //查询条件为出发点,目的地,时间，出游天数,酒店等级，按价格排序
+                else if (null == TDATA)
+                    rowkey = SP + "HHH" + EP + TRAFFIC + HGRADE + "%";
+                else if(null == HGRADE)
+                    rowkey = SP + "HHH" + EP + TRAFFIC + "%" +TDATA + "天%";
+                else if(null == TRAFFIC)
                     rowkey = SP + "AAA" + EP + TDATA + "天" + HGRADE + "%";
+                else                                          //查询条件为出发点,目的地,时间，出游天数,酒店等级，按价格排序
+                    rowkey = SP + "HHH" + EP + TRAFFIC + HGRADE +TDATA + "天%";
 
                 if ("DESC".equals(sort)) {  //降序
                     System.out.println(99 + rowkey);
-                    startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVEL WHERE ROWKEY LIKE ? ORDER BY ROWKEY DESC LIMIT 1 ", Integer.class, rowkey);
+                    startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVELTESTS WHERE ROWKEY LIKE ? ORDER BY ROWKEY DESC LIMIT 1 ", Integer.class, rowkey);
                     endkey = startRecord - (currentPageNum - 1) * pageSize;
                     startkey = startRecord - (currentPageNum) * pageSize;
                 } else {  //升序（默认）
                     System.out.println(rowkey + "   mobin");
-                    startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVEL WHERE  ? LIMIT 1 ", Integer.class, rowkey);
+                    startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVELTESTS WHERE  ? LIMIT 1 ", Integer.class, rowkey);
                     startkey = startRecord + (currentPageNum - 1) * pageSize;
                     endkey = startRecord + (currentPageNum) * pageSize;
                 }
 
             } else {  //根据性价比排序
                 String r = "ROWKEY LIKE ";
-                if ((null == HGRADE && null == TDATA) || ("".equals(HGRADE) && "".equals(TDATA)))    //参考上面if语句
+                if ((null == HGRADE && null == TDATA && TRAFFIC == null))    //参考上面if语句
                     rowkey = SP + EP + "%";
-                else if (null == HGRADE)
+                else if(null == HGRADE && null == TDATA)
+                    rowkey = SP + "KKK" + EP + TRAFFIC + "%";
+                else if(null == HGRADE && null == TRAFFIC)
                     rowkey = SP + "DDD" + EP + TDATA + "天%";
+                else if (null == TDATA && null == TRAFFIC)
+                    rowkey = SP + "CCC" + EP +HGRADE + "%";
                 else if (null == TDATA)
-                    rowkey = SP + "CCC" + EP + HGRADE + "%";
-                else
+                    rowkey = SP + "LLL" + EP + TRAFFIC + HGRADE + "%";
+                else if(null == HGRADE)
+                    rowkey = SP + "LLL" + EP + TRAFFIC + "%" +TDATA + "天%";
+                else if(null == TRAFFIC)
                     rowkey = SP + "BBB" + EP + TDATA + "天" + HGRADE + "%";
+                else
+                    rowkey = SP + "LLL" + EP + TRAFFIC + HGRADE +TDATA + "天%";
 
                 System.out.println(rowkey + "KPOP");
                 //得到行记录数
-                startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVEL WHERE  " + r + "  ? LIMIT 1", Integer.class, rowkey);
+                startRecord = jdbcTemplate.queryForObject("SELECT RECORDID FROM TRAVELTESTS WHERE  " + r + "  ? LIMIT 1", Integer.class, rowkey);
                 startkey = startRecord + (currentPageNum - 1) * pageSize;
                 endkey = startRecord + (currentPageNum) * pageSize;
 
             }
-            findPageRecords_sql = "SELECT ROWKEY FROM TRAVEL WHERE ROWKEY LIKE ? AND RECORDID >= ? AND RECORDID < ? ";
+            findPageRecords_sql = "SELECT ROWKEY FROM TRAVELTESTS WHERE ROWKEY LIKE "+"'"+""+rowkey+""+"'"+" AND RECORDID >= ? AND RECORDID < ? ";
         }
             isSortByPrice = (null == sort) ? "" : "ORDER BY PRICE " + sort;
 
@@ -144,10 +176,10 @@ public class TravelDaoImpl implements TravelDao {
                                 sb.append("'"+subRowkey+ "'");
                             return 0;
                         }
-                    }, rowkey,startkey,endkey);
+                    },startkey,endkey);
 
         //TODO  去掉ROWKEY
-        String sql1 = "SELECT ROWKEY,URL,SP,EP,TITLE,TOUATT,ST,TDATA,PRICE,TRAFFIC,RETURN,TTYPE,IMAGE,PROXY,HOTEL,ORIGIN FROM TRAVEL WHERE ROWKEY IN ("+sb.toString()+")  " +isSortByPrice;
+        String sql1 = "SELECT ROWKEY,URL,SP,EP,TITLE,TOUATT,ST,TDATA,PRICE,TRAFFIC,RETURN,TTYPE,IMAGE,PROXY,HOTEL,ORIGIN FROM TRAVELTESTS WHERE ROWKEY IN ("+sb.toString()+")  " +isSortByPrice;
 
         List<Travel> travels = jdbcTemplate.query(sql1,
                     new RowMapper<Travel>() {
@@ -162,16 +194,16 @@ public class TravelDaoImpl implements TravelDao {
     public int getTotalRecords() {
 
         if(rk1== null && rk2 == null)////当按价格区间进行筛选时其count(*)语句不同于其他情况
-            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVEL WHERE  ROWKEY LIKE ? ", Integer.class, rowkey);
+            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVELTESTS WHERE  ROWKEY LIKE ? ", Integer.class, rowkey);
         else
-            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVEL WHERE  ROWKEY >= ? AND ROWKEY <= ?", Integer.class, rk1,rk2);
+            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVELTESTS WHERE  ROWKEY >= ? AND ROWKEY <= ?", Integer.class, rk1,rk2);
     }
 
     public int getTotalRecords(String SP) {
         if(SP.equals("all")){
-            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVEL",Integer.class);
+            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVELTESTS",Integer.class);
         }else{
-           return  jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVEL WHERE SP=?",Integer.class,SP);
+           return  jdbcTemplate.queryForObject("SELECT COUNT(*) FROM TRAVELTESTS WHERE SP=?",Integer.class,SP);
         }
     }
 
